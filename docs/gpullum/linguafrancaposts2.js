fe7fd94ac42328c26a263a44723710a52040866a
Project4Dimensions <project4dimensions@users.noreply.github.com>
2022-08-12 14:43:13 +0100

    Update 2022-08-12T14:45

--- docs/gpullum/linguafrancaposts2.js
+++ docs/gpullum/linguafrancaposts2.js
@@ -6,19 +6,12 @@ function lfpList() {
         let td1 = document.createElement("td");
         let td2 = document.createElement("td");
         let td3 = document.createElement("td");
-        //let font = document.createElement("font");
         let aTitle = document.createElement("a");
 
-        //td1.setAttribute("valign", "top");
-        //td2.setAttribute("valign", "top");
-        //td3.setAttribute("valign", "top");
-        //font.setAttribute("size", "+1");
         aTitle.setAttribute("href", obj.b_link);
 
         td1.appendChild(document.createTextNode(obj.a_num));
         aTitle.appendChild(document.createTextNode(obj.c_title));
-        //font.appendChild(aTitle);
-        //td2.appendChild(font);
         td2.appendChild(aTitle);
         td3.appendChild(document.createTextNode(obj.d_dateUK));
 
