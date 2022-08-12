ec76d7afc63b1cadfa03e39df7940c4236d29f49
Project4Dimensions <project4dimensions@users.noreply.github.com>
2022-08-12 06:29:16 +0100

    Update 2022-08-12T06:30

--- docs/gpullum/linguafrancaposts2.js
+++ docs/gpullum/linguafrancaposts2.js
@@ -1,24 +1,25 @@
 function lfpList() {
     "use strict";
+    const posts = document.getElementById("lfPosts");
     lfp3.map(function (obj) {
-        let posts = document.getElementById("lfPosts");
         let tr = document.createElement("tr");
         let td1 = document.createElement("td");
         let td2 = document.createElement("td");
         let td3 = document.createElement("td");
-        let font = document.createElement("font");
+        //let font = document.createElement("font");
         let aTitle = document.createElement("a");
 
-        td1.setAttribute("valign", "top");
-        td2.setAttribute("valign", "top");
-        td3.setAttribute("valign", "top");
-        font.setAttribute("size", "+1");
+        //td1.setAttribute("valign", "top");
+        //td2.setAttribute("valign", "top");
+        //td3.setAttribute("valign", "top");
+        //font.setAttribute("size", "+1");
         aTitle.setAttribute("href", obj.b_link);
 
         td1.appendChild(document.createTextNode(obj.a_num));
         aTitle.appendChild(document.createTextNode(obj.c_title));
-        font.appendChild(aTitle);
-        td2.appendChild(font);
+        //font.appendChild(aTitle);
+        //td2.appendChild(font);
+        td2.appendChild(aTitle);
         td3.appendChild(document.createTextNode(obj.d_dateUK));
 
         tr.appendChild(td1);
@@ -33,7 +34,6 @@ function lfpList() {
  * Options
  *   Env…
  *     browser
- *     devel
  *   imported globals
  *     lfp3
  * Report: Warnings (0)
